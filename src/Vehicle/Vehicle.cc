54b102b63b0793d0847cea402e4ff689dd4de9f1
Brad Stinson <bradstinson@gmail.com>
2024-10-15 12:36:38 -0400

    add set_zoom to commands which can be duplicated

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -3150,6 +3150,8 @@ bool Vehicle::_commandCanBeDuplicated(MAV_CMD command)
     switch (command) {
     case MAV_CMD_DO_MOTOR_TEST:
         return true;
+    case MAV_CMD_SET_CAMERA_ZOOM:
+        return true;
     default:
         return false;
     }
