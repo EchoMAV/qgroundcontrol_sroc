bbb791c093f21793a48b9c2b8aead7db4ba1663d
Brad Stinson <brad@echomav.com>
2024-10-21 08:36:41 -0400

    1.0.3, includes Optical Flow mode 99

--- src/FirmwarePlugin/APM/ArduCopterFirmwarePlugin.h
+++ src/FirmwarePlugin/APM/ArduCopterFirmwarePlugin.h
@@ -49,6 +49,7 @@ public:
         AUTOROTATE  = 26,
         AUTO_RTL    = 27,
         TURTLE      = 28,
+        OPTICAL_FLOW = 99,
     };
 
     APMCopterMode(uint32_t mode, bool settable);
--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -16,7 +16,7 @@
  *
  */
 
-#define ECHOMAV_VERSION "v1.0.2"
+#define ECHOMAV_VERSION "v1.0.3"
 
 #include <QFile>
 #include <QRegularExpression>
