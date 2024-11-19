addaca532a719fcfffe1c2bd7b4bd8a06655d802
Benjamin Castor <bcastor@echomav.com>
2024-11-19 12:20:26 -0500

    update numbers

--- src/FirmwarePlugin/APM/ArduCopterFirmwarePlugin.h
+++ src/FirmwarePlugin/APM/ArduCopterFirmwarePlugin.h
@@ -42,14 +42,14 @@ public:
         AVOID_ADSB  = 19,
         GUIDED_NOGPS= 20,
         SMART_RTL   = 21,  // SMART_RTL returns to home by retracing its steps
-        FLOWHOLD    = 99,  // This truly points to EchoMAV "Optical Flow" mode and not stock "FLOWHOLD" mode
+        FLOWHOLD    = 22,  // This truly points to custom "OPTICAL_FLOW" in EchoMAV ArduPilot
         FOLLOW      = 23,  // follow attempts to follow another vehicle or ground station
         ZIGZAG      = 24,  // ZIGZAG mode is able to fly in a zigzag manner with predefined point A and point B
         SYSTEMID    = 25,
         AUTOROTATE  = 26,
         AUTO_RTL    = 27,
         TURTLE      = 28,
-        OPTICAL_FLOW = 22, // This is the stock "FLOWHOLD" mode which is essentially unused in EchoMAV ArduPilot
+        OPTICAL_FLOW = 99, // This truly points to stock "FLOWHOLD" in EchoMAV ArduPilot
     };
 
     APMCopterMode(uint32_t mode, bool settable);
