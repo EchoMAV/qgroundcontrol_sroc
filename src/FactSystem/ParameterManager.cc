e07f5c9a0f03a5fc61ae017f4a20832ae44e27f5
Thomas Lyons <tlyons@echomav.com>
2025-01-29 11:21:42 -0500

    working on pairing

--- src/FactSystem/ParameterManager.cc
+++ src/FactSystem/ParameterManager.cc
@@ -18,7 +18,7 @@
 #include "ComponentInformationManager.h"
 #include "CompInfoParam.h"
 #include "FTPManager.h"
-
+#include "Monark/MonarkManager.h"
 #include <QEasingCurve>
 #include <QFile>
 #include <QDebug>
